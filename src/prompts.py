b36a6a7481be9875d9c527fc71f23c002dc94d55
Peter Vanden Berghe <peter.vandenberghe@ordina.be>
2024-10-01 14:49:31 +0200

    working question to query translation

---
+++ src/prompts.py
@@ -0,0 +1,21 @@
+QUESTION_TO_QUERY_PROMPT = """You are an AI assistant specialized in transforming dutch natural language questions to dutch queries.
+You can use the following operators:
+- `OR`: or-operator, term1 OR term2
+- `AND`: and-operator, term1 AND term2
+- `"`: proximity search, "term1 term2"
+- <field_name>:<keywords>, "location:<location1>"
+
+# Here is the schema of the database:
+{schema}
+
+# Examples:
+Input:
+marktplaats in leuven
+Output:
+markt AND location:leuven
+
+# Question to be translated into a query (use dutch to answer):
+Input:
+{question}
+Output:
+"""
---
+++ src/schema.py
@@ -0,0 +1,34 @@
+from whoosh.fields import Schema
+from pydantic import BaseModel
+from whoosh.fields import Schema, TEXT, ID
+from whoosh.analysis import StemmingAnalyzer
+
+
+ID_KEY = 'id'
+PRODUCT_NAME_KEY = 'product_name'
+DESCRIPTION_KEY = 'description'
+LOCATION_KEY = 'location'
+
+
+CATALOG_SCHEMA = Schema(
+        id=ID(stored=True),
+        product_name=TEXT(stored=True),
+        description=TEXT(stored=True, analyzer=StemmingAnalyzer()),
+        location=TEXT(stored=True),
+    )
+
+# this will be used by the llm to understand the schema of the data
+CATALOG_SCHEMA_DESCRIPTIONS = {
+    ID_KEY: 'The id of the item.',
+    PRODUCT_NAME_KEY: 'The name of the product catalog item/service.',
+    DESCRIPTION_KEY: 'The high-level description of the product catalog item.',
+    LOCATION_KEY: 'The location/municipality that offers the service.',
+}
+
+
+ALL_SEARCH_FIELDS = (PRODUCT_NAME_KEY, DESCRIPTION_KEY, LOCATION_KEY)
+
+class SearchResult(BaseModel):
+    product_name: str
+    location: str
+    
