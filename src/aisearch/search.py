6a154672099623c6e6b743970d5182beda02fd1f
Peter Vanden Berghe <peter.vandenberghe@ordina.be>
2024-10-01 23:55:08 +0200

    work on search so it returns results data

--- src/aisearch/search.py
+++ src/aisearch/search.py
@@ -5,28 +5,35 @@ from whoosh.qparser import MultifieldParser
 
 from aisearch.schema import ALL_SEARCH_FIELDS
 
-DATA_DIR = Path('/home/peter/data')
+DATA_DIR = Path(__file__).parent.parent.parent / 'data'
 INDEX_DIR = DATA_DIR / 'index'
 INDEX = open_dir(INDEX_DIR)
 
 PARSER = MultifieldParser(ALL_SEARCH_FIELDS, INDEX.schema)
 
 
-def search(query: str):
+def search(query: str, limit: int = 10):
     query = PARSER.parse(query)
     print(query)
 
     with INDEX.searcher() as searcher:
-        results = searcher.search(query)
-        return results
+        results = searcher.search(query, limit=limit)
+
+        results_data = []
+        for result in results:
+            data = dict(result.items())
+            data.update({'score': result.score})
+            results_data.append(data)
+
+        return results_data
 
 
 if __name__ == '__main__':
-    queries = ['location:leuven', 'voorlopig_rijbewijs AND location:aarschot']
+    queries = ['location:leuven', '"voorlopig rijbewijs" AND location:aarschot']
 
     for query in queries:
         results = search(query=query)
         print('-' * 50)
         print(f'Question: {query}')
-        print(f'Results:\n{results}')
+        print(f'Results ({len(results)}):\n{results}')
         print('-' * 50)
