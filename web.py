7a563e01622437d1f67af26af487d5a0e65b08e6
Peter Vanden Berghe <Peter.VandenBerghe@soprasteria.com>
2024-10-01 22:38:59 +0000

    add dev running for app

--- web.py
+++ web.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/test-connection')
 def hello():
     return 'ai-search-service is running.'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5005)
